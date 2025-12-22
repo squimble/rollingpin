2b3b3abba6b5a05c51dc2c26c5d689a3528ee2f3
squimble <mojglobus34@gmail.com>
2025-12-22 11:44:43 -0500

    add header in main.user.js

---
+++ main.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         RollingPin
+// @namespace    rollingpin
+// @version      1
+// @description  Cookie Clicker automater
+// @author       squimble
+// @match        https://orteil.dashnet.org/cookieclicker/
+// @match        http://orteil.dashnet.org/cookieclicker/
+// @homepageURL  https://github.com/squimble/rollingpin
+// @supportURL   https://github.com/squimble/rollingpin/issues
+// ==/UserScript==
